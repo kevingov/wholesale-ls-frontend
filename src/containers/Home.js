67142074dbd941a4077fd6725fa1bd14d4a0df6f
Kevin Gov <kevingov@LAP00393-MacBook-Pro.local>
2020-07-14 17:04:42 -0400

    Worked on how it worked cards

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -18,9 +18,9 @@ export default function Home() {
           <Jumbotron>
             <div className="container">
               <h1>Find your Next Investment Property</h1>
-              <p>Wholesale Properties for Flip, BRRRR, or Resale</p>
-
-                <FormGroup>
+              <p>Wholesale Contracts for Flip, BRRRR, or Resale</p>
+              <Button variant="success" href="#">Search Properties</Button>
+                {/* <FormGroup>
                   <Row>
                     <Col xs={6}>
                       <FormControl placeholder="Location" />
@@ -29,7 +29,9 @@ export default function Home() {
                       <FormControl placeholder="Max Price" />
                     </Col>
                   </Row>
-                </FormGroup>
+                </FormGroup> */}
+
+
             </div>
           </Jumbotron>
       </div>
@@ -58,7 +60,9 @@ export default function Home() {
 
 
       <div className="cityCards">
-        <h1>Search your Favourite Cities</h1>
+        <h1>Featured Cities</h1>
+        <h2>Search your Favourite Cities</h2>
+        <p>Browse the best deals in the Hottest Markets</p>
           <Row>
             <Col xs="6" sm="4" className="text-center" >
               <Card className="text-center" as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
@@ -102,10 +106,115 @@ export default function Home() {
 
       </div>
 
+      <div className="about-us-container">
+        <Row className="about-us-text">
+          <Col xs={6}>
+            <h1>About Us</h1>
+            <h3>Welcome to the First Real Estate Marketplace for Off-Market Properties. </h3>
+
+              <h3>We Work exclusively with Real Estate Wholesalers to find you undervalued property that are ready to flip in one convenience platform. </h3>
+
+              <h3>Instead of scouring through MLS or your Real Estate Broker for deals, come browse our selection of private home owners in need of a quick sale. 
+            </h3>
+            <div class="title-underline"></div>
+          </Col>
+          <Col xs={6}>
+            <img src="https://wholesale-ls-marketing.s3.amazonaws.com/Modena-keys2.jpeg"></img>
+          </Col>
+        </Row>
+      </div>
+
+      <div className="how-it-works">
+        <Row>
+          <Col xs={6}>
+            <h1>How it works</h1>
+            <h3>We'll help streamline the entire transaction.</h3>
+          </Col>
+        </Row>
+      </div>
+      <div className="grid-template">                
+        <Row>
+          
+            <Col xs={6}>
+                <Card className="browse-property" as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
+                  <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Modena-browse+property.jpg" />
+                    <CardBody>
+                      <CardTitle>Find your Ideal Contract</CardTitle>
+                        <CardText tag="p">
+                          Search through our entire listing of property contracts to find one that suits your criterias.
+                        </CardText>
+
+                    </CardBody>
+                </Card>  
+            </Col>
+            <Col xs={6}>
+              <Row>
+                <Col xs={6}>
+                  <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
+                      <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Modena-Message.jpg" />
+                        <CardBody>
+                          <CardTitle>Message the Seller</CardTitle>
+                            <CardText tag="p">
+                              Inquiry directly with the wholeseller regarding the contract. 
+                            </CardText>
+
+                        </CardBody>
+                  </Card>  
+                </Col>   
+
+                 <Col xs={6}>
+                    <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
+                        <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/modena-houseviewing.jpg" />
+                          <CardBody>
+                            <CardTitle>Schedule a Showing</CardTitle>
+                              <CardText tag="p">
+                                Coordinate a time to view the property in person prior to purchase. 
+                              </CardText>
+
+                          </CardBody>
+                    </Card>  
+                </Col>             
+              </Row>
+              <Row>
+                <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
+                  <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Modena-Transaction.jpg" />
+                    <CardBody>
+                      <CardTitle>Purchase the Contract</CardTitle>
+                        <CardText tag="p">
+                          Finalize the details and purchase the contract from the wholeseller. 
+                        </CardText>
+
+                    </CardBody>
+                </Card>            
+              </Row>
+            </Col>
+          
+          </Row>
+        </div>
+
+      <div className="newsletter">
+        
+          <div className="green-bg">
+           </div>
+         <div className="newsletter-box">
+          <h1>Subscribe to our Newsletter</h1>
+         </div>
+
+           
+           
+      <div className="image-bg">
+        
+      </div>  
+             
+         
+       
+      </div>
+      
+
       <div className="featured-property">
         <h1>Featured Properties</h1>
             <Row>
-              <Col>
+              <Col md={3}>
                 <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
                   <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
                     <CardBody>
@@ -117,7 +226,31 @@ export default function Home() {
                     </CardBody>
                 </Card>  
               </Col>
-              <Col>
+              <Col md={3}>
+                <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
+                  <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
+                    <CardBody>
+                      <CardTitle>Ottawa</CardTitle>
+                        <CardText tag="p">
+                          Export your Leads to your favorite CRM to analyze which campaigns are the most effective.
+                        </CardText>
+                          {/* <Button variant="primary">See Properties</Button> */}
+                    </CardBody>
+                </Card>
+              </Col>   
+              <Col md={3}>
+                <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
+                  <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
+                    <CardBody>
+                      <CardTitle>Ottawa</CardTitle>
+                        <CardText tag="p">
+                          Export your Leads to your favorite CRM to analyze which campaigns are the most effective.
+                        </CardText>
+                          {/* <Button variant="primary">See Properties</Button> */}
+                    </CardBody>
+                </Card>
+              </Col>   
+              <Col md={3}>
                 <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
                   <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
                     <CardBody>
@@ -133,63 +266,7 @@ export default function Home() {
       </div>
 
 
-      <div className="about-us-container">
-        <Row className="about-us-text">
-          <Col xs={6} md={4}>
-            <h1>About Us</h1>
-            <h3>Welcome to the First Real Estate Marketplace for Off-Market Properties.
-
-              We Work exclusively with Real Estate Wholesalers to find you undervalued property that are ready to flip in one convenience platform.
-
-              Instead of scouring through MLS or your Real Estate Broker for deals, come browse our selection of private home owners in need of a quick sale. 
-            </h3>
-          </Col>
-        </Row>
-
-        <Row>
-
-          <Col xs={6}>
-            <Row>
-              <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
-                  <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
-                    <CardBody>
-                      <CardTitle>Ottawa</CardTitle>
-                        {/* <CardText tag="p">
-                          Export your Leads to your favorite CRM to analyze which campaigns are the most effective.
-                        </CardText> */}
-                    </CardBody>
-              </Card>
-            </Row>
-          </Col>
-
-
-          <Col xs={6}>
-              <Row>
-                <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
-                    <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
-                      <CardBody>
-                        <CardTitle>Ottawa</CardTitle>
-                          {/* <CardText tag="p">
-                            Export your Leads to your favorite CRM to analyze which campaigns are the most effective.
-                          </CardText> */}
-                      </CardBody>
-                </Card>     
-              </Row>
-              <Row>
-              <Card as="a" onClick={"/properties"} style={{ cursor: "pointer"}}>
-                    <CardImg variant="top" src="https://wholesale-ls-marketing.s3.amazonaws.com/Ottawa+skyline.jpg" />
-                      <CardBody>
-                        <CardTitle>Ottawa</CardTitle>
-                          {/* <CardText tag="p">
-                            Export your Leads to your favorite CRM to analyze which campaigns are the most effective.
-                          </CardText> */}
-                      </CardBody>
-                </Card> 
-              </Row>
-          
-          </Col>
-        </Row>
-      </div>
+      
 
     </div>
 
