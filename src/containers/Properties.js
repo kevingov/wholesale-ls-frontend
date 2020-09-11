44662aa38c349b59fb9858d49d52b9cd9c14dfaf
Kevin Gov <kevingov@LAP00393-MacBook-Pro.local>
2020-09-11 12:09:33 -0400

    got sort to work

--- src/containers/Properties.js
+++ src/containers/Properties.js
@@ -59,8 +59,19 @@ export default function Properties(props) {
     setFilterSort(event.value);
   }
 
+  // function sortProperties (filterSort) {
+  //   if (filterSort === "Newest") {
+  //     return sort((a, b) => b.createdAt - a.createdAt)
+  //   } else if (filterSort === "Oldest") {
+  //     return sort((a, b) => a.createdAt - b.createdAt)
+  //   } else if (filterSort === "Price High-Low") {
+  //     return sort((a, b) => a.price - b.price) 
+  //   } else {
+  //     return sort((a, b) => b.price - a.price)
+  //     }
+  //   }
   
-  const filterDropdown = properties.filter( results => {         
+  const resultsFilteredByDropdown = properties.filter(results => {         
     console.log(filterPropertyType);
     if (filterPropertyType === "") {
       return results
@@ -69,7 +80,18 @@ export default function Properties(props) {
         && results.bedroom > filterBedrooms
         && results.bathroom > filterBathrooms)
     }
-    });
+  });
+
+  let sortedProperties;
+  if (filterSort === "Newest") {
+    sortedProperties = resultsFilteredByDropdown.sort((a, b) => b.createdAt - a.createdAt)
+  } else if (filterSort === "Oldest") {
+    sortedProperties = resultsFilteredByDropdown.sort((a, b) => a.createdAt - b.createdAt)
+  } else if (filterSort === "Price High-Low") {
+    sortedProperties = resultsFilteredByDropdown.sort((a, b) => Number(a.price) - Number(b.price)) 
+  } else {
+    sortedProperties = resultsFilteredByDropdown.sort((a, b) => Number(b.price) - Number(a.price))
+  }
 
   return (
     <div className="Index">
@@ -86,256 +108,257 @@ export default function Properties(props) {
             <Row className="Filters">
               <Col xs={3}>
                 <FormGroup controlId="filterPropertyType">
-                        <br />
-                        {/* <FormControl 
-                          type="text" 
-                          placeholder="Search" 
-                          onChange={ e => setFilter(e.target.value)}>
-                        </FormControl> */}
-                        <br />
-                        <br />
+                  <br />
+                  {/* <FormControl 
+                    type="text" 
+                    placeholder="Search" 
+                    onChange={ e => setFilter(e.target.value)}>
+                  </FormControl> */}
+                  <br />
+                  <br />
                 </FormGroup>
               </Col>
               <Col xs={1}>
               </Col>
               <Col xs={2}>
               <FormGroup controlId="filterPropertyType">
-                        <br />
-                        <Dropdown
-                          placeholder="Property Types"
-                          value={filterPropertyType}
-                          options={[
-                            {
-                              label: "Detached",
-                              value: "Detached",
-                            },
-                            {
-                              label: "Semi-Detached",
-                              value: "Semi-Detached",
-                            },
-                            {
-                              label: "Townhome",
-                              value: "Townhome",
-                            },
-                            {
-                              label: "Condo",
-                              value: "Condo",
-                            },
-                            {
-                              label: "Multi-family",
-                              value: "Multi-family",
-                            },
-                          ]}
-                          onChange={updateFilterPropertyType}
-                        />
-                        <br />
-                        <br />
-                </FormGroup>
+                <br />
+                <Dropdown
+                  placeholder="Property Types"
+                  value={filterPropertyType}
+                  options={[
+                    {
+                      label: "Detached",
+                      value: "Detached",
+                    },
+                    {
+                      label: "Semi-Detached",
+                      value: "Semi-Detached",
+                    },
+                    {
+                      label: "Townhome",
+                      value: "Townhome",
+                    },
+                    {
+                      label: "Condo",
+                      value: "Condo",
+                    },
+                    {
+                      label: "Multi-family",
+                      value: "Multi-family",
+                    },
+                  ]}
+                  onChange={updateFilterPropertyType}
+                />
+                <br />
+                <br />
+              </FormGroup>
                 
               </Col>
               <Col xs={2}>
                 <FormGroup controlId="filterBedrooms">
-                          <br />
-                          <Dropdown
-                            placeholder="Bedrooms"
-                            value={filterBedrooms}
-                            options={[
-                              {
-                                label: "Any",
-                                value: 0,
-                              },
-                              {
-                                label: "2+",
-                                value: 2,
-                              },
-                              {
-                                label: "3+",
-                                value: 3,
-                              },
-                              {
-                                label: "4+",
-                                value: 4,
-                              },
-                            ]}
-                            onChange={updateFilterBedrooms}
-                          />
-                          <br />
-                          <br />
+                  <br />
+                  <Dropdown
+                    placeholder="Bedrooms"
+                    value={filterBedrooms}
+                    options={[
+                      {
+                        label: "Any",
+                        value: 0,
+                      },
+                      {
+                        label: "2+",
+                        value: 2,
+                      },
+                      {
+                        label: "3+",
+                        value: 3,
+                      },
+                      {
+                        label: "4+",
+                        value: 4,
+                      },
+                    ]}
+                    onChange={updateFilterBedrooms}
+                  />
+                  <br />
+                  <br />
                 </FormGroup>
               </Col>
               <Col xs={2}>
                 <FormGroup controlId="filterBathrooms">
-                          <br />
-                          <Dropdown
-                            placeholder="Bathrooms"
-                            value={filterBathrooms}
-                            options={[
-                              {
-                                label: "Any",
-                                value: 0,
-                              },
-                              {
-                                label: "2+",
-                                value: 2,
-                              },
-                              {
-                                label: "3+",
-                                value: 3,
-                              },
-                              {
-                                label: "4+",
-                                value: 4,
-                              },
-                            ]}
-                            onChange={updateFilterBathrooms}
-                          />
-                          <br />
-                          <br />
+                  <br />
+                  <Dropdown
+                    placeholder="Bathrooms"
+                    value={filterBathrooms}
+                    options={[
+                      {
+                        label: "Any",
+                        value: 0,
+                      },
+                      {
+                        label: "2+",
+                        value: 2,
+                      },
+                      {
+                        label: "3+",
+                        value: 3,
+                      },
+                      {
+                        label: "4+",
+                        value: 4,
+                      },
+                    ]}
+                    onChange={updateFilterBathrooms}
+                  />
+                  <br />
+                  <br />
                 </FormGroup>
               </Col>
               <Col xs={2}>
               <FormGroup controlId="filterSort">
-                        <br />
-                        <Dropdown
-                          placeholder="Sort By"
-                          value={filterSort}
-                          options={[
-                            {
-                              label: "Newest",
-                              value: "Newest",
-                            },
-                            {
-                              label: "Oldest",
-                              value: "Oldest",
-                            },
-                            {
-                              label: "Price High-Low",
-                              value: "Price High-Low",
-                            },
-                            {
-                              label: "Price Low-High",
-                              value: "Price Low-High",
-                            },
-                          ]}
-                          onChange={updateFilterSort}
-                        />
-                        <br />
-                        <br />
+                <br />
+                <Dropdown
+                  placeholder="Sort By"
+                  value={filterSort}
+                  options={[
+                    {
+                      label: "Newest",
+                      value: "Newest",
+                    },
+                    {
+                      label: "Oldest",
+                      value: "Oldest",
+                    },
+                    {
+                      label: "Price High-Low",
+                      value: "Price High-Low",
+                    },
+                    {
+                      label: "Price Low-High",
+                      value: "Price Low-High",
+                    },
+                  ]}
+                  onChange={updateFilterSort}
+                />
+                <br />
+                <br />
                 </FormGroup>
               </Col>
             </Row>
 
-              {filterDropdown.map((property, i) => (
-                <Col key={i} sm={4}>
-                <a href={`/properties/${property.propertyId}`}>
-                  <div className="Property">
-                    <div
-                      style={{
-                        backgroundImage: `url(https://${config.s3.BUCKET}.s3.amazonaws.com/public/${property.image})`,
-                        backgroundSize: "cover",
-                        height: "200px",
-                        borderRadius: "5px",
-                        marginBottom: "5px",
-                      }}
-                      className="propertyImage"
-                    ></div>
-                    <div className="Property-Title">
-                      <p>
-                          {property.title.length > 45
-                          ? property.title.slice(0, 45) + " ..."
-                          : property.title}
-                      </p>
-                    </div>
-                    
-                      <div className="Price">
-                        <p>$ {property.price}</p>
-                      </div>
-                    
-                    
-                    <Row className="Row-Highlights">
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.bedroom} Bedrooms</p>
-                      </Col>
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.bathroom} Bathrooms</p>
-                      </Col>
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.propertyType} </p>
-                      </Col>
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.city} </p>
-                      </Col>
-                    </Row>
+            {sortedProperties 
+            .map((property, i) => (
+              <Col key={i} sm={4}>
+              <a href={`/properties/${property.propertyId}`}>
+                <div className="Property">
+                  <div
+                    style={{
+                      backgroundImage: `url(https://${config.s3.BUCKET}.s3.amazonaws.com/public/${property.image})`,
+                      backgroundSize: "cover",
+                      height: "200px",
+                      borderRadius: "5px",
+                      marginBottom: "5px",
+                    }}
+                    className="propertyImage"
+                  ></div>
+                  <div className="Property-Title">
+                    <p>
+                        {property.title.length > 45
+                        ? property.title.slice(0, 45) + " ..."
+                        : property.title}
+                    </p>
                   </div>
                   
-                </a>
-                <br />
-                <br />
-              </Col>
-              ))}
+                    <div className="Price">
+                      <p>$ {property.price}</p>
+                    </div>
+                  
+                  
+                  <Row className="Row-Highlights">
+                    <Col xs={3} className="Property-Highlights">
+                      <p> {property.bedroom} Bedrooms</p>
+                    </Col>
+                    <Col xs={3} className="Property-Highlights">
+                      <p> {property.bathroom} Bathrooms</p>
+                    </Col>
+                    <Col xs={3} className="Property-Highlights">
+                      <p> {property.propertyType} </p>
+                    </Col>
+                    <Col xs={3} className="Property-Highlights">
+                      <p> {property.city} </p>
+                    </Col>
+                  </Row>
+                </div>
+                
+              </a>
+              <br />
+              <br />
+            </Col>
+            ))}
 
 Everything Above here should be a filter
 
-                    <br />
-                    <br />
+            <br />
+            <br />
             
 
 
 
 
-          {properties
-            .sort((a, b) => b.createdAt - a.createdAt)
-            .map((property, i) => (
-              <Col key={i} sm={4}>
-                <a href={`/properties/${property.propertyId}`}>
-                  <div className="Property">
-                    <div
-                      style={{
-                        backgroundImage: `url(https://${config.s3.BUCKET}.s3.amazonaws.com/public/${property.image})`,
-                        backgroundSize: "cover",
-                        height: "200px",
-                        borderRadius: "5px",
-                        marginBottom: "5px",
-                      }}
-                      className="propertyImage"
-                    ></div>
-                    <div className="Property-Title">
-                      <p>
-                          {property.title.length > 45
-                          ? property.title.slice(0, 45) + " ..."
-                          : property.title}
-                      </p>
-                    </div>
-                    {/* <p>
-                        {property.propertyType} &middot; {property.city}
-                    </p> */}
-                    
+            {properties
+              .sort((a, b) => b.createdAt - a.createdAt)
+              .map((property, i) => (
+                <Col key={i} sm={4}>
+                  <a href={`/properties/${property.propertyId}`}>
+                    <div className="Property">
+                      <div
+                        style={{
+                          backgroundImage: `url(https://${config.s3.BUCKET}.s3.amazonaws.com/public/${property.image})`,
+                          backgroundSize: "cover",
+                          height: "200px",
+                          borderRadius: "5px",
+                          marginBottom: "5px",
+                        }}
+                        className="propertyImage"
+                      ></div>
+                      <div className="Property-Title">
+                        <p>
+                            {property.title.length > 45
+                            ? property.title.slice(0, 45) + " ..."
+                            : property.title}
+                        </p>
+                      </div>
+                      {/* <p>
+                          {property.propertyType} &middot; {property.city}
+                      </p> */}
+                      
                       <div className="Price">
                         <p>$ {property.price}</p>
                       </div>
+                      
+                      
+                      <Row className="Row-Highlights">
+                        <Col xs={3} className="Property-Highlights">
+                          <p> {property.bedroom} Bedrooms</p>
+                        </Col>
+                        <Col xs={3} className="Property-Highlights">
+                          <p> {property.bathroom} Bathrooms</p>
+                        </Col>
+                        <Col xs={3} className="Property-Highlights">
+                          <p> {property.propertyType} </p>
+                        </Col>
+                        <Col xs={3} className="Property-Highlights">
+                          <p> {property.city} </p>
+                        </Col>
+                      </Row>
+                    </div>
                     
-                    
-                    <Row className="Row-Highlights">
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.bedroom} Bedrooms</p>
-                      </Col>
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.bathroom} Bathrooms</p>
-                      </Col>
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.propertyType} </p>
-                      </Col>
-                      <Col xs={3} className="Property-Highlights">
-                        <p> {property.city} </p>
-                      </Col>
-                    </Row>
-                  </div>
-                  
-                </a>
-                <br />
-                <br />
-              </Col>
-            ))}
+                  </a>
+                  <br />
+                  <br />
+                </Col>
+          ))}
         </Row>
       ) : (
         <Loading />
