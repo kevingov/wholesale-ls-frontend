5d27b4bff1cc18cb1ae132c4be17325bc8b29e92
Jon Mak <makjonathan@hotmail.com>
2021-07-02 13:49:22 -0400

    prevent duplicate conversations w/ same participants

--- src/helper.js
+++ src/helper.js
@@ -26,3 +26,13 @@ export const numberWithCommas = (x) => {
   if (x === null) return "XXX,XXX";
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+// attach the .equals method to Array's prototype to compare if 2 Arrays contain the same elements
+Array.prototype.equals = function (array) {
+  if (!array) return false;
+  if (this.length != array.length) return false;
+
+  return this.every((item) => array.includes(item));
+};
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
